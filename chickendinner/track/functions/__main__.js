f0146edc43626433c1df18e153dfd1d5c833212f
abu ahamed <abusahamed@gmail.com>
2018-03-31 21:01:49 -0400

    average

--- chickendinner/track/functions/__main__.js
+++ chickendinner/track/functions/__main__.js
@@ -1,5 +1,4 @@
 // const scraber = require('../help/scraber');
-const axios = require('axios')
 const jsdom = require('jsdom')
 const { JSDOM } = jsdom  
 const request = require('request')
@@ -7,16 +6,23 @@ const request = require('request')
 /**
 * A basic Hello World function
 * @param {string} url Who you're saying hello to
-* @returns {string}
+* @param {any} price
+* @returns {any}
 */
-module.exports = (url, context, callback) => {
-    // request(url, function (error, response, body) {
-    // const tags = new JSDOM(body).window.document.getElementsByClassName("lvprice")
-    // let newArr = []
-    // for (let tag of tags) {
-    //     newArr.push(parseInt(tag.children[0].innerHTML.replace(/[^0-9.]|/g, '')))
-    // }
-    console.log(typeof url)
-    // callback(null, url)
-// });
+module.exports = (url, price, context, callback) => {
+    request(url, function (error, response, body) {
+    const tags = new JSDOM(body).window.document.getElementsByClassName("lvprice")
+    let prices = []
+    let underPrices = []
+    for (let tag of tags) {
+        prices.push(parseInt(tag.children[0].innerHTML.replace(/[^0-9.]|/g, '')))
+        if (parseInt(tag.children[0].innerHTML.replace(/[^0-9.]|/g, '')) <= parseInt(price)) {
+            underPrices.push(tag.childNodes)  
+        }
+
+    }
+    
+    const average = prices.reduce((sum, num) => sum + num, 0) / prices.length;
+    callback(null, {average, price, underPrices})
+});
 }
