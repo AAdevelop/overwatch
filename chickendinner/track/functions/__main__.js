583adf1ce6015eb2c8c8e09a60bba87f2e36fef6
ridhwaan any <ridhwaan.any@gmail.com>
2018-03-31 21:10:32 -0400

    Add sms

--- chickendinner/track/functions/__main__.js
+++ chickendinner/track/functions/__main__.js
@@ -2,6 +2,11 @@
 const jsdom = require('jsdom')
 const { JSDOM } = jsdom  
 const request = require('request')
+const twilio = require('twilio');
+const sid = 'ACfc64229b1f785fcab95e4d97c05f07a2';
+const authToken = 'e72bb59749a2d8c7668cd1d44372ebca';
+const phonenumber = '+12672456091';
+
 
 /**
 * A basic Hello World function
@@ -25,4 +30,15 @@ module.exports = (url, price, context, callback) => {
     const average = prices.reduce((sum, num) => sum + num, 0) / prices.length;
     callback(null, {average, price, underPrices})
 });
+}
+
+let smsclient = new twilio(sid,authToken);
+function sendmsg (body,to){
+	    smsclient.messages.create({
+		        body: 'Are you coming to school today',
+		        to: '+16095800390',
+		        from: phonenumber
+	    },function(message){
+		        console.log(message);
+		        }); 
 }
