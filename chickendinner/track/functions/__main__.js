bcd553f6456605af4744c5a00d209c89aec14c2d
ridhwaan any <ridhwaan.any@gmail.com>
2018-04-01 01:08:24 -0400

    update ebay api

--- chickendinner/track/functions/__main__.js
+++ chickendinner/track/functions/__main__.js
@@ -14,36 +14,34 @@ const request = require('request')
 * @param {any} price
 * @returns {any}
 */
-// module.exports = (itemString, price, context, callback) => {
-    // get ebay item:
- 
+module.exports = (itemString, price, context, callback) => {
 
-//     request(url, function (error, response, body) {
-//     const tags = new JSDOM(body).window.document.getElementsByClassName("lvprice")
-//     const items = new JSDOM(body).window.document.getElementById('ListViewInner')
-//     console.log(items);
-//     let prices = []
-//     let underPrices = []
-//     for (let tag of tags) {
-//         prices.push(parseInt(tag.children[0].innerHTML.replace(/[^0-9.]|/g, '')))
-//         if (parseInt(tag.children[0].innerHTML.replace(/[^0-9.]|/g, '')) <= parseInt(price)) {
-//             underPrices.push(tag.childNodes)  
-//         }
-
-//     }
+    request(url, function (error, response, body) {
+    const tags = new JSDOM(body).window.document.getElementsByClassName("lvprice")
+    const items = new JSDOM(body).window.document.getElementById('ListViewInner')
+    console.log(items);
+    let prices = []
+    let underPrices = []
+    for (let tag of tags) {
+        prices.push(parseInt(tag.children[0].innerHTML.replace(/[^0-9.]|/g, '')))
+        if (parseInt(tag.children[0].innerHTML.replace(/[^0-9.]|/g, '')) <= parseInt(price)) {
+            underPrices.push(tag.childNodes)  
+        }
+    }
     
-//     const average = prices.reduce((sum, num) => sum + num, 0) / prices.length;
+    const average = prices.reduce((sum, num) => sum + num, 0) / prices.length;
 
-//     callback(null, {average, price, underPrices})
-// });
-// }
-
-
+    callback(null, {average, price, underPrices})
+});
+}
+let itemString = 'Nintendo Switch Console'
+let min = 300;
+let max = 800
 request({ headers: {
-    'Authorization':'Bearer v^1.1#i^1#f^0#p^3#r^0#I^3#t^H4sIAAAAAAAAAOVXe2wURRzu9QFpgAKBIAqSY2lAafZudu/29nblTq8t0Aq0pddSqCF1dne23bK3e9nZbXskmHJR1PBI1cRESbQhSgJihCgIiQ+MVKIh/gESfOADHwHRGFATjYBx9vq61gBtIaaJ989mZn6v7/t9MzcDOicULt5SseWPKZ6Jud2doDPX42EmgcIJBSVFebl3FeSALANPd2dxZ34678ISDBN6UqxFOGkaGHk7ErqBxcxkhHIsQzQh1rBowATCoi2L8diqlSLrA2LSMm1TNnXKW1keoYICw6qqygcELsCxPEdmjf6YdWaEYkFYCMtQ5mFIDcjIXcfYQZUGtqFhu+tMmAZBGjB1gBVZTmQYX4gJNlLeNcjCmmkQEx+goplyxYyvlVXrjUuFGCPLJkGoaGVsWbw6Vlm+tKpuiT8rVrSPh7gNbQcPHZWZCvKugbqDbpwGZ6zFuCPLCGPKH+3NMDSoGOsvZgzl91ItS0owJISkYIhXUIC5LVQuM60EtG9chzujKbSaMRWRYWt26maMEjakViTbfaMqEqKy3Ot+VjtQ11QNWRFqaWlsXX18aS3ljdfUWGabpiDFRcowXDAYZkEoREUJo8iQkakmoQUVDaO+ZL0R+6gelq3MNBTNJQ57q0y7FJHK0XB+mCx+iFG1UW3FVNutKtsu2M8jCDW6je3tpGO3GG5vUYKQ4c0Mb96FflkMCuF2CYOVWaiEQrwMEODVkDpEGO5eH6M4om5/YjU1frcWJMEUnYDWBmQndSgjWib0OglkaYoY4FQ2EFYRrYQElQ4KqkpLnBKiGRUhgJAkyUL4/6YR27Y0ybHRgE6GL2SARqi4bCZRjalrcooabpI5e/pU0YEjVIttJ0W/v7293dce8JlWs58FgPGvXbUyLregBKQGbLWbG9NaRh8yIl5YE+1UklTTQeRHkhvNVDRgKTXQslOlToqM40jXyadfwkMqjA6fvQ5U7EIdXyBdf0wCwKTmcxXuk82E34RkR7tTTZmKvSMx8ktOiuRXkOWzEFRMQ0+N3K/ZQdju9R6ZEybd8PVuRgJjIKO718cSYBQ+mtFGtGxaqVHCHOo8Ch8oy6Zj2GNJ1+c6Cg/V0VVN193tOpaEWe6jKdOAesrWZDyWlFknMqEXa80t9mjjkDlyjBN/GdpQNwfldEubPZZMVirja7PXakpLO4QGXQHlDTX1dLx0La3yYT7ACKxKI44PhIWBP6qxoS5Hbf8R6vx07iMjRs7JPCsJgkSHWImngxLP0pBjWRohgedVDnIMG7gl3GW6RkRflxpv53uFiW2k3Bo0ctUaX6Bc3fbLVpWgQIdVoNDBMKPQUAjwtIxCwkgh+697W/nXZdU/9MUYzcn8mLTnbZD2HCGPTsADmikB907Iq8/Pm0xhzUY+DA1FMjt8GlR95HQyyIPIQr4NKJWEmpU7wfPQnIv3X816q3avB7MHXquFecykrKcrmDu4UsBMvWMKEwZBwACW5RimESwYXM1nZuXPPNujvzNn67U3YEPXqdPhqyvYyTN/A1MGjDyegpz8tCen1ZN4f/eBM/P8pVveO/fSjG+n4ctTS7kVm546eWLR4fPfr57W8MHDC/aWn8Nfvzn3UknPzoaDF/cveKDs6Uf3VBznP3tlWSP3zAtfrW2av2J99Rnn2i/7ZhjFqWffagXar/OPzIx/voPfuHHd8vWHjoLOLw6kn191+lTX63sO/ri7+uwx/67pjxUVPXc8Ii16fM/df6nbO3Z/M6t4ZcOnL27fvHzTydf2X410bq3ozn2VPr/th5O7tk3yLJz34Yljwp2/P/nl4q7Cn70/LdzcdL7p3Y89rfnpy8e471KXug7tqO2p7il+eWd91YUrRXtrC/+ceoWd2DF351FdK4kvOTv9wcO77rvn77bZH+37JEd7wkqbvW38B6Ly7d5FEAAA',
+	'Authorization':'Bearer v^1.1#i^1#r^0#f^0#p^1#I^3#t^H4sIAAAAAAAAAOVXf2wTVRxf222G4FATGIxssdz8FfCu7653be+kNWXdskXYStstsgXm69277bb2rt67spX4x5iRGCFOAROEQBpj4jSaOIkICTEBMfoHCREUfxDjr4AkEhMCcZka9O5aRjcJTJhAYv9p3vd93/d9Pp/v9/vePTBUOWfppuZN41WOu5z5ITDkdDjouWBOZcWyeS7n4ooyUOLgyA89MFQ+7Dq7HMN0KiPEEM5oKkbuwXRKxYJtDBJZXRU0iBUsqDCNsGCIQjy8aqXAUEDI6JqhiVqKcLdEggQHeE4KyJKPQYzMMJJpVS/HTGhBAoo8En0+L6JRgEe0OY1xFrWo2ICqESQYQAdIwJKATgBWYHkBsBTH8Z2EuwPpWNFU04UCRMhGK9hr9RKo10YKMUa6YQYhQi3hpnhbuCXS2JpY7imJFSrKEDegkcVTRw2ahNwdMJVF194G295CPCuKCGPCEyrsMDWoEL4M5gbg20qLYjIpI46W/Kws8155VqRs0vQ0NK6Nw7IoEinbrgJSDcXIXU9RU41kHxKN4qjVDNEScVt/q7MwpcgK0oNE44rwmnA0SoRiitQ7AKFKNkOxP9pORmMRkvcH/H4aQkTyTIDhkAiK2xRiFUWetk+DpkqKJRl2t2rGCmRiRtOV8ZYoYzq1qW16WDYsPKV+vkkFuU4rpYUcZo1e1coqSpsyuO3h9fWfXG0YupLMGmgywvQJWyCzZzIZRSKmT9qVWCyeQRwkeg0jI3g8AwMD1ICX0vQeDwMA7Xly1cq42IvSkCj4Wr1u+ivXX0AqNhURmSuxIhi5jIll0KxUE4DaQ4QYv4/3c0Xdp8IKTbf+w1DC2TO1H2arPzivn0WA8fkYBoo0z81Gf4SKJeqxcKAkzJFpqPcjI5OCIiJFs86yaaQrkuDlZMYbkBEp+XiZZHlZJpOc5CNpGSGAUDIp8oH/T5vMtNDjopZBUS2liLnZKvfZKXWvLkWhbuTiKJUyDTOt+auSxBbJW0PP6vWZUrRiYDMIzCiUVdmUqKU9GjSPNMvUbaO+Kd6KeRfeUUk1CRaYKlLhEqNsuhReL1I6wlpWN+9vqs061RNaP1LNLjF0LZVCegd9U0rM6nl+O87yq7ISU4opY/edxuxfHZI3WNnQuP2cy4cdiVLeNMcwHBegWfamuDXYWU3kbtWRNdO0NmvYQNJ/8PHhmfoOCpXZP3rY8T4YdoyZTyngAQ/S9WBJpau93HX3YqwYiFKgTGGlRzW/73VE9aNcBiq6s9LRVfvum90lL6/8WrBo8u01x0XPLXmIgdorMxX0PQur6ABgAQ1YlgdsJ6i/MltOV5fP3/EHxe06UeHfzdQczB88exYuUl8HVZNODkdFmVkSZf1dj4/s25LfKquemnUNuYmaU1/UXIz9yY6eqVjY992e/bXBnZd+73pkXsfR9ILvR1o/2Rce9UQWDE6cofKHR5Tq40vrt7/QfezRyrBr/xv7Ry8d2Z56tXf3y3UTzgtPHXNu/AVsPefMr+9z/TV67ujDGyKN7Wt3/vRB18H6bRePnf6h/cvDI+fGvk4rKw5tPL4hOt7zYnWkLrl691uHjtY+duC+C/eef+WJyJKP3nbt/VYOVJ1ec7L1wETnz4d2tCQj1eMXx8Z7t75zfmzZqcOxb15qOvn52s2xxEODe7d1ts+79Gzf5rm/3f/r8qZnat/7+KsPGz6bHziybgu35/nVr1X9WFf99NLnPo017tpyYk1dIX1/A2lKcgoTDwAA',
     'Content-Type':'application/json',
-    'X-EBAY-C-ENDUSERCTX':'affiliateCampaignId=<ePNCampaignId>,affiliateReferenceId=<referenceId>'
-    }, url: `https://api.sandbox.ebay.com/buy/browse/v1/item_summary/search?q=nintendo+switch&filter=buyingOptions:{FIXED_PRICE}&filter=price:[100]&limit=50`, 
+    'X-EBAY-C-ENDUSERCTX':'affiliateCampaignId=<ePNCampaignId>,affiliateReferenceId=<referenceId>',
+    }, url: `https://api.ebay.com/buy/browse/v1/item_summary/search?q=${itemString}&limit=20&filter=price:[${min}..${max}],priceCurrency:USD`, 
       method:'GET'}, function(err,res,body){
        console.log(body);
     });
